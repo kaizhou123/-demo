996a5cf919344a2e20d522eaf02a90e58753685b
QSteve <775393943@qq.com>
2020-11-18 16:41:03 +0800

    no message

---
+++ CustomPlayerView/CustomPlayerView/adsasd.h
@@ -0,0 +1,17 @@
+//
+//  adsasd.h
+//  CustomPlayerView
+//
+//  Created by QSteve on 11/18/20.
+//  Copyright © 2020 MrZhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface adsasd : QMUITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CustomPlayerView/CustomPlayerView/adsasd.m
@@ -0,0 +1,13 @@
+//
+//  adsasd.m
+//  CustomPlayerView
+//
+//  Created by QSteve on 11/18/20.
+//  Copyright © 2020 MrZhou. All rights reserved.
+//
+
+#import "adsasd.h"
+
+@implementation adsasd
+
+@end
