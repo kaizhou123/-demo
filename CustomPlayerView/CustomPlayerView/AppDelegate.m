78a231af89b627f1a06fb661dc6bd49305f6c83f
QSteve <775393943@qq.com>
2020-11-18 16:36:04 +0800

    no message

--- CustomPlayerView/CustomPlayerView/AppDelegate.m
+++ CustomPlayerView/CustomPlayerView/AppDelegate.m
@@ -17,6 +17,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    ///123123123132
+    
     return YES;
 }
 
